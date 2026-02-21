68f4c95f53eaddf55f4548a5d3ac1f79a1e5dbcc
balasundary <balasundary2005@gmail.com>
2026-02-21 19:32:40 +0530

    Time: 3 ms (87.47%), Space: 43.4 MB (58.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        java.util.Deque<Character> stack = new java.util.ArrayDeque<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
